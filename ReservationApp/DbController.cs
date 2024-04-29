d13a0dbc4b19cb6734c7e137d0a9c62985b79910
María Vargas <alejavd2205@gmail.com>
2024-04-29 17:41:56 -0500

    Se agregó el método CrearArchivoenCarpeta.

--- ReservationApp/DbController.cs
+++ ReservationApp/DbController.cs
@@ -20,6 +20,21 @@ public class DbController
         {
             Console.WriteLine($"Ocurrió un error al intentar crear la carpeta: {ex.Message}");
         }
+
+        // Nombre del archivo que deseas crear
+        string nombreArchivo = "MiArchivo.txt";
+
+        try
+        {
+            // Llamar al método para crear el archivo
+            CrearArchivoEnCarpeta(nombreArchivo);
+
+            Console.WriteLine($"El archivo '{nombreArchivo}' se ha creado exitosamente.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ocurrió un error al intentar crear el archivo: {ex.Message}");
+        }
     }
 
     static void CrearCarpetaEnDirectorioUsuario(string nombreCarpeta)
@@ -41,4 +56,43 @@ public class DbController
             throw new InvalidOperationException($"La carpeta '{nombreCarpeta}' ya existe en {rutaCarpeta}");
         }
     }
+
+    static void CrearArchivoEnCarpeta(string nombreArchivo)
+    {
+        try
+        {
+            // Obtener la ruta base del directorio del usuario actual
+            string rutaBase = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            // Concatenar la ruta base con el nombre de la carpeta "MiCarpeta"
+            string rutaCarpeta = Path.Combine(rutaBase, "MiCarpeta");
+
+            // Verificar si la carpeta "Archivos" no existe antes de intentar crearla
+            if (!Directory.Exists(rutaCarpeta))
+            {
+                // Crear la carpeta "Archivos"
+                Directory.CreateDirectory(rutaCarpeta);
+            }
+
+            // Concatenar la ruta de la carpeta "Archivos" con el nombre del archivo
+            string rutaArchivo = Path.Combine(rutaCarpeta, nombreArchivo);
+
+            // Verificar si el archivo no existe antes de intentar crearlo
+            if (!File.Exists(rutaArchivo))
+            {
+                // Crear el archivo y cerrarlo inmediatamente
+                using (File.Create(rutaArchivo)) { }
+            }
+            else
+            {
+                throw new InvalidOperationException($"El archivo '{nombreArchivo}' ya existe en {rutaArchivo}");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException($"Ocurrió un error al intentar crear el archivo: {ex.Message}");
+        }
+    }
+
+
 }
