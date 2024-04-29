3d2999e13d0bc22dc6fdb840f22eb4ff40847e66
Jacobo Rodriguez Guevara <jacobo1304@gmail.com>
2024-04-29 13:57:48 -0500

    Ahora si, se corrigio el mal push y se creo la estructura de la clase booking.

--- ReservationApp/Booking.cs
+++ ReservationApp/Booking.cs
@@ -3,8 +3,30 @@ namespace ReservationApp;
 public class Booking
 {
 
-    new Client client;
-    private int bookingId
+    private Client _client;
+    public Client GetClient() => _client;
+    private Room _room;
+    public Room GetRoom() => _room;
+    public Room Room => _room;
+    private int _bookingId;
+    
+    public int GetBookingId() => _bookingId;
+
+    public int BookingId => _bookingId;
+    
+    private bool _expired;
+    public bool Getstate() => _expired;
+    public bool expired => _expired;
+   
+    private int _bookedNights;
+
+    public int GetBookedNights() => _bookedNights;
+    public int BookedNights => _bookedNights;  
+    private float price;
+
+  
+
+  
     
 }
 
