086b7de36a96a59dc9ee04387a55f69626a5ca2d
Jacobo Rodriguez Guevara <jacobo1304@gmail.com>
2024-04-29 23:21:05 -0500

    Se realizo la estructura de la clase client

--- ReservationApp/Client.cs
+++ ReservationApp/Client.cs
@@ -2,5 +2,12 @@ namespace ReservationApp;
 
 public class Client
 {
-    
+private int _clientID;
+public int GetClientID() => _clientID;
+public int ClientID => _clientID; //ClientID es una propiedad de solo lectura
+private string _clientName;
+
+public string GetClientName() => _clientName;
+public string ClientName => _clientName;
+
 }
