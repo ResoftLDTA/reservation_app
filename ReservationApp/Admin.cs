1d40d45e30514b2e949c7358f6522701deb93a97
santiagoco55 <168220931+santiagoco55@users.noreply.github.com>
2024-05-06 15:34:27 -0500

    Se implemento la herencia a admin y el boceto del metodo CalculateIncome

--- ReservationApp/Admin.cs
+++ ReservationApp/Admin.cs
@@ -1,6 +1,13 @@
 namespace ReservationApp;
 
-public class Admin
+public class Admin : Staff
 {
-    
+    public float CalculateIncome(uint month)
+    {
+    float income = 0
+
+    // luego se reemplaza el valor 0 de income por el proceso final para calcularlo segun month
+
+    return income;
+    }
 }
